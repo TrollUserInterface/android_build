cde8de7ddccff5a0857a1e80fd75052ac4ac0268
Cıyanogen <ttbpccorporation@outlook.com>
2018-07-22 19:14:04 +0300

    Some changes v2

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -709,13 +709,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("*          Feel my hands in your hair :)           *");
   script.Print("****************************************************");
   
-  if GetBuildProp("ro.viper.version", OPTIONS.info_dict) is not None:
-    buildid = GetBuildProp("ro.viper.version", OPTIONS.info_dict)
+  if GetBuildProp("ro.ecem.version", OPTIONS.info_dict) is not None:
+    buildid = GetBuildProp("ro.ecem.version", OPTIONS.info_dict)
     buildidn = GetBuildProp("ro.build.id", OPTIONS.info_dict)
     buildday = GetBuildProp("ro.build.date", OPTIONS.info_dict)
     securep = GetBuildProp("ro.build.version.security_patch", OPTIONS.info_dict)
     density = GetBuildProp("ro.sf.lcd_density", OPTIONS.info_dict,False)
-    device = GetBuildProp("ro.viper.device", OPTIONS.info_dict)
+    device = GetBuildProp("ro.ecem.device", OPTIONS.info_dict)
     androidver = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
     manufacturer = GetBuildProp("ro.product.manufacturer", OPTIONS.info_dict)
     sdkver = GetBuildProp("ro.build.version.sdk", OPTIONS.info_dict)
